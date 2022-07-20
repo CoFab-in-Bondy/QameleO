93e73e065379bebdab04d06ddbb38148de7eeebc
Dashstrom <incerti.felix@gmail.com>
2022-07-20 10:44:20 +0200

    wrap define in ifndef

--- software/QameleO/QameleO_conf.h
+++ software/QameleO/QameleO_conf.h
@@ -1,46 +1,108 @@
 #ifndef QAMELEO_CONF_H
 #define QAMELEO_CONF_H
 
+
 // 1 - DEFINE THE NAME OF THE QameleO ACCORDING to the DATABASE
-#define SENSOR_NAME "TEST_"
+
+//#define SENSOR_NAME "Name"
+#ifndef SENSOR_NAME
+    #error You must enter a name
+#endif
 
 // 2 - DEFINE QameleO INTERNAL COMPONENTS
 
+// Configuration
 //#define DHT_SENSOR
-#define SHT_SENSOR
+//#define SHT_SENSOR
 
-#define NextPM_SENSOR
+//#define NextPM_SENSOR
 //#define PMS7003_SENSOR
+
+// Default values
+#ifndef DHT_SENSOR
+    #define SHT_SENSOR
+#endif
+#ifndef PMS7003_SENSOR
+    #define NextPM_SENSOR
+#endif
+
+// Check duplicate
+#ifdef DHT_SENSOR
+    #ifdef SHT_SENSOR
+        #error Must specify only one of NextPM_SENSOR and PMS7003_SENSOR
+    #endif
+#endif
+#ifdef NextPM_SENSOR
+    #ifdef PMS7003_SENSOR
+        #error Must specify only one of NextPM_SENSOR and PMS7003_SENSOR
+    #endif
+#endif
 
 #define CONFIG_MEGA
  
+
 // 3 - DEFINE log system
-#define FILE_NAME_LOG "datalog.csv"           //Contient les données envoyées en GSM    //"datalog.csv"
-#define FILE_NAME_BUFFER "notsend.csv"        //Contient les données qui n'ont pas encore été envoyé en GSM  //"databuffer.csv"
-#define FILE_NAME_SAVE "timesave.csv"     //Contient les temps effectués par le programme en cas de reboot
+
+// Contient les données envoyées en GSM
+#ifndef FILE_NAME_LOG
+    #define FILE_NAME_LOG "datalog.csv"
+#endif
+
+// Contient les données qui n'ont pas encore été envoyé en GSM
+#ifndef FILE_NAME_BUFFER
+    #define FILE_NAME_BUFFER "notsend.csv"
+#endif
+
+// Contient les temps effectués par le programme en cas de reboot
+#ifndef FILE_NAME_SAVE
+    #define FILE_NAME_SAVE "timesave.csv"
+#endif
 
 
 // 4 - DEFINE GSM Connection
+
 // Set Sim card pin code
-#define GSM_PIN_CODE "0000"
-#define GSM_APN "orange"
-#define GSM_USER ""
-#define GSM_PASS ""
-#define GSM_MQTT_BROKER "vmpams.ird.fr"
+#ifndef GSM_PIN_CODE
+    #define GSM_PIN_CODE "0000"
+#endif
+#ifndef GSM_APN
+    #define GSM_APN "orange"
+#endif
+#ifndef GSM_USER
+    #define GSM_USER ""
+#endif
+#ifndef GSM_PASS
+    #define GSM_PASS ""
+#endif
+#ifndef GSM_MQTT_BROKER
+    #define GSM_MQTT_BROKER "vmpams.ird.fr"
+#endif
 
-// Set phone numbers, if you want to test SMS and Calls
-//#define SMS_TARGET  "+33688334906" //"+33638231637"
-//#define CALL_TARGET "+33688334906" //"+33638231637"
-// Set phone connection timeout
-//#define NETWORK_REGISTER_TIMEOUT 60000L
-
-
-
-#define MEASURE_PERIOD 1            // Time in minutes     //default = 15
-#define SEND_PERIOD 4               // Time in minutes     //default = 60
-#define RECEIVE_UNIXTIME_PERIOD 240 // Time in minutes     //default = 240
-
+#ifndef MEASURE_PERIOD
+    #define MEASURE_PERIOD 1            // Time in minutes  default = 15
+#endif
+#ifndef SEND_PERIOD
+    #define SEND_PERIOD 4               // Time in minutes  default = 60
+#endif
+#ifndef RECEIVE_UNIXTIME_PERIOD
+    #define RECEIVE_UNIXTIME_PERIOD 240 // Time in minutes  default = 240
+#endif
 
 // XXX - DEFINE REBOOT DELAY
-#define REBOOT_DELAY 5 
+#ifndef REBOOT_DELAY
+    #define REBOOT_DELAY 5
+#endif
+
+
+// Configuration for send message
+#ifndef SMS_TARGET
+  #define SMS_TARGET  "+33688334906" // "+33638231637"
+#endif
+#ifndef CALL_TARGET
+  #define CALL_TARGET "+33688334906" // "+33638231637"
+#endif
+#ifndef NETWORK_REGISTER_TIMEOUT
+  #define NETWORK_REGISTER_TIMEOUT 60000L
+#endif
+
 #endif // QAMELEO_CONF_H
--- software/QameleO/QameleO_hard_conf.h
+++ software/QameleO/QameleO_hard_conf.h
@@ -4,70 +4,54 @@
 #include "QameleO_conf.h"
 
 // 1 - Configuration GSM
-  #define GSM_OFF 9
-  #define GSM_LOWP 2
-  #define GSM_ON 3
-  #define GSM_RESET 13
-  #define TINY_GSM_MODEM_SIM800
-  
-  #ifndef GSM_PIN_CODE
-    #define GSM_PIN_CODE "0000"
-  #endif  
-  #ifndef SMS_TARGET
-    #define SMS_TARGET  "+33688334906" //"+33638231637"
-  #endif
-  #ifndef CALL_TARGET
-    #define CALL_TARGET "+33688334906" //"+33638231637"
-  #endif
-  #ifndef NETWORK_REGISTER_TIMEOUT
-    #define NETWORK_REGISTER_TIMEOUT 60000L
-  #endif
+#define GSM_OFF 9
+#define GSM_LOWP 2
+#define GSM_ON 3
+#define GSM_RESET 13
+#define TINY_GSM_MODEM_SIM800
 
- /* 
-  * A vérifier mais je ne pense pas que ça soit la place pour le mettre avec le SoftwareSerial */
-  #ifdef CONFIG_MEGA
+// A vérifier mais je ne pense pas que ça soit la place pour le mettre avec le SoftwareSerial
+#ifdef CONFIG_MEGA
   #define SerialAT Serial2
   #define TRANSMISSION_RATE 9600 //115200   //9600
-  #endif
+#endif
 /*
-  #ifdef CONFIG_UNO
-  SoftwareSerial SerialAT(10, 11);
-  #define TRANSMISSION_RATE 9600
-  #endif
+#ifdef CONFIG_UNO
+SoftwareSerial SerialAT(10, 11);
+#define TRANSMISSION_RATE 9600
+#endif
 */
 
 // 2 - Configuration Fan
-  #define FAN_CONTROL_PIN 6
-  #define FAN_DELAY 10000 //12000//0
+#define FAN_CONTROL_PIN 6
+#define FAN_DELAY 10000 //12000//0
 
 // 3 - Configuration SD
-  #define SD_CONTROL_PIN 53
+#define SD_CONTROL_PIN 53
 
 // 4 - Configuration Humidity & Temperature Sensor
-  #define HUMIDITY_TEMP_SWITCH_PIN 7
+#define HUMIDITY_TEMP_SWITCH_PIN 7
 
-  //4.1 - DHT22_Sensor
-    #define DHT11_PIN 4
-    #define DHTTYPE DHT22
-  
+//4.1 - DHT22_Sensor
+#define DHT11_PIN 4
+#define DHTTYPE DHT22
 
-  //4.2 - SHT35_Sensor
-    #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-      #define SDAPIN  20
-      #define SCLPIN  21
-      #define RSTPIN  7
-      #define SERIAL SerialUSB
-    #else
-      #define SDAPIN  A4
-      #define SCLPIN  A5
-      #define RSTPIN  2
-      #define SERIAL Serial
-    #endif 
-  
+//4.2 - SHT35_Sensor
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+  #define SDAPIN  20
+  #define SCLPIN  21
+  #define RSTPIN  7
+  #define SERIAL SerialUSB
+#else
+  #define SDAPIN  A4
+  #define SCLPIN  A5
+  #define RSTPIN  2
+  #define SERIAL Serial
+#endif 
 
 // 5 - Configuration Dust Sensor
-  #define PMS_CONTROL_PIN 8
-  #define PMS_ACQUIRE_DELAY 60000
-  #define SerialPMS Serial1
+#define PMS_CONTROL_PIN 8
+#define PMS_ACQUIRE_DELAY 60000
+#define SerialPMS Serial1
   
 #endif // QAMELEO_HARD_CONF_H
